654208b2a377a9fd42a8fb2d45d70eb590816361
Jean Caio <111510390+jeancaiocs@users.noreply.github.com>
2025-11-27 09:16:23 -0300

    front vercel

--- src/main/java/com/jeancaio/financecontrol/config/CorsConfig.java
+++ src/main/java/com/jeancaio/financecontrol/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://fin-cas-frontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
